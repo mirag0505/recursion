6777d67dde0228849a924b2d1b4311cec41d20b3
alex_di <mirag0505@yandex.ru>
2023-08-17 18:04:25 +0300

    fix

--- 1/4.cs
+++ 1/4.cs
@@ -7,19 +7,18 @@ namespace Recursion
 {
     public class Task4
     {
+        private static bool recursionIterator(string text, int indexOffset)
+        {
+            int endIndex = text.Length - indexOffset - 1;
+            int startIndex = indexOffset;
+
+            if (startIndex >= endIndex) return true;
+            if (text[startIndex] != text[endIndex]) return false;
+            return recursionIterator(text, indexOffset + 1);
+        }
+
         public static bool isPalindrom(string text)
         {
-
-            static bool recursionIterator(string text, int indexOffset)
-            {
-                int endIndex = text.Length - indexOffset - 1;
-                int startIndex = indexOffset;
-
-                if (startIndex >= endIndex) return true;
-                if (text[startIndex] != text[endIndex]) return false;
-                return recursionIterator(text, indexOffset + 1);
-            }
-
             return recursionIterator(text, 0);
         }
     }
